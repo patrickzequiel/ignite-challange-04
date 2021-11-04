91403f15695f01f45ed87ccd411d5a03fb877b30
Patrick Cruz <patrickezequielscruz@gmail.com>
2021-11-04 17:05:24 +0100

    Subindo header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,20 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+import Image from 'next/image';
+
+import Logo from '../../../public/images/Logo.svg';
+
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <>
+      <header className={styles.container}>
+        <div className={styles.container__content}>
+          <Link href="/">
+            <Image src={Logo} alt="logo" width={238.62} height={25.63} />
+          </Link>
+        </div>
+      </header>
+    </>
+  );
 }
---
+++ src/pages/post/index.tsx
@@ -0,0 +1,7 @@
+export function Post(): JSX.Element {
+  return (
+    <div>
+      <h1>Post</h1>
+    </div>
+  );
+}
